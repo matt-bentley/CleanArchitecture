b516b7a33d17dbace32846ef52d10cb0cf969da4
Matt Bentley <mabentley@deloitte.co.uk>
2023-09-05 19:37:32 +0100

    added code styles

--- src/CleanArchitecture.Core/IsExternalInit.cs
+++ src/CleanArchitecture.Core/IsExternalInit.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using System.ComponentModel;
+
 namespace System.Runtime.CompilerServices
 {
-    using System.ComponentModel;
     /// <summary>
     /// Reserved to be used by the compiler for tracking metadata.
     /// This class should not be used by developers in source code.
